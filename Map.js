57a0ca12b37b79604e33459ea31ad27bd9e929cf
Mikroh <mikael.holsve@live.dk>
2015-01-29 14:24:11 +0100

    Design/Map/Default

--- Map.js
+++ Map.js
@@ -1,1 +1,18 @@
-﻿
+﻿var mapCanvas = document.querySelector("#map-canvas");
+
+if (!navigator) {
+    mapCanvas.innerHTML = "Denne browser understøtter ikke GeoLocation.";
+}
+else {
+    function c(pos) {
+        var lat = pos.coords.latitude;
+        var long = pos.coords.longitude;
+
+        var mapOptions = {
+            center: {}
+        };
+    }
+
+    navigator.geolocation.getCurrentPosition(c);
+
+}
